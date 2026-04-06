d1c8ea7bc64b88c36b0563805a1595fcffac0458
Self-Portrait with Death Playing the Fiddle <nile.internal.auditor@gmail.com>
2026-04-06 19:42:08 +0200

    Add files via upload

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,1 +1,36 @@
+function render(data) {
+    const totals = {};
 
+    data.forEach(r => {
+        totals[r.Card] = (totals[r.Card] || 0) + (r.Amount || 0);
+    });
+
+    const ctx = document.getElementById("chart");
+
+    if (window.chart) window.chart.destroy();
+
+    window.chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: Object.keys(totals),
+            datasets: [{
+                label: 'Fuel Consumption',
+                data: Object.values(totals)
+            }]
+        }
+    });
+}
+
+// Live updates
+chrome.storage.onChanged.addListener((changes) => {
+    if (changes.transactions_live) {
+        render(changes.transactions_live.newValue);
+    }
+});
+
+// Initial load
+chrome.storage.local.get("transactions_live", res => {
+    if (res.transactions_live) {
+        render(res.transactions_live);
+    }
+});
